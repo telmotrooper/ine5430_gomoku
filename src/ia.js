acd256fc663e66bea5d68152d23da6a4b288b73b
joaquim.belo <@joaquim.belo>
2018-04-06 15:51:46 -0300

    Removed matrix copy

--- src/ia.js
+++ src/ia.js
@@ -474,13 +474,13 @@ class IA {
 		let value = 0;
 		if( color == iaColor ){
 			if( length > 1 ){
-				value += this.amp( length ) * this.mod( spaces, blocked );
+				value += this.group( length ) * this.modifier( spaces, blocked );
 			}else{
 				value++;
 			}
 		}else{
 			if( length > 1 ){
-				value -= this.amp( length ) * this.mod( spaces, blocked );
+				value -= this.group( length ) * this.modifier( spaces, blocked );
 			}else{
 				value--;
 			}
@@ -488,7 +488,7 @@ class IA {
 		return value;
 	}
 
-	amp( length ){
+	group( length ){
 		let value = 0;
 		switch (length){
 			case 2:
@@ -506,79 +506,8 @@ class IA {
 		return value;
 	}
 
-	mod( spaces, blocked ){
-		return 10 - spaces - (blocked * 5);
-	}
-
-	getComputerColor(lastMove) {	// Finds out the color of the pieces controlled by the computer
-		let color = null;
-		if(lastMove == null) {
-			color = 1;	// The first to play is always black (value: 1)
-		} else {
-			switch (lastMove[2]) {	// color of the last move
-			case 0:	// blank
-				console.log("THIS SHOULD NEVER HAPPEN!");
-				break;
-			case 1:	// player is black, computer is white
-				color = 2;
-				break;
-			case 2:	// player is white, computer is black
-				color = 1;
-			}
-		}
-		return color;
-	}
-
-	getInverseColor(color) {
-		switch (color) {
-		case 1:
-			return 2;
-		case 2:
-			return 1;
-		}
-	}
-
-	copyMatrix(matrix) {
-		return JSON.parse(JSON.stringify(matrix));
-	}
-
-	countSequence(color, array) {	// Used inside checkVictory()
-		let sequences = [];
-		let lineValue = 0;
-		let counter = 0;
-
-		for(let i = 0; i < array.length; i++) {
-			if(array[i] == color) {
-				counter += 1;
-			} else {
-				if(counter > 0) {
-					if(counter == 5) {
-						sequences.push(300 * 150 * 75 * 50);
-					} else if(counter == 4) {
-						sequences.push(300 * 150 * 75);
-					} else if(counter == 3) {
-						sequences.push(300 * 150);
-					} else if(counter == 2) {
-						sequences.push(300);
-					} else {
-						sequences.push(counter);
-					}
-				}
-				counter = 0;
-			}
-		}
-
-		if(sequences.length > 0) {
-			lineValue = sequences.reduce((a, b) => {return a + b;});
-
-			/* Decrementing the sequence value for every
-			   break between sequences including being blocked */
-			for(let i = 0; i < sequences.length - 1; i++) {
-				lineValue *= 0.75;
-			}
-		}
-
-		return lineValue;
+	modifier( spaces, blocked ){
+		return 10 - spaces - (blocked * 4);
 	}
 
 	minimax(node, depth, alpha, beta, maximizing)
@@ -589,14 +518,14 @@ class IA {
 		let bestValue = Number.MIN_SAFE_INTEGER;
 
 		for(let i = 0; i < children.length; i++) {
-			let myNode = this.copyMatrix(node);
 
 			let x = children[i][0];
 			let y = children[i][1];
 
-			myNode[x][y] = this.computerColor;
+			node[x][y] = this.computerColor;
+			v = this.minimaxNext(node, depth - 1, alpha, beta, false);
+			node[x][y] = 0;
 
-			v = this.minimaxNext(myNode, depth - 1, alpha, beta, false);
 			if(bestValue < v) {
 				bestPlay = [];
 				bestValue = v;
@@ -624,36 +553,31 @@ class IA {
 			let children = this.getEmptySlots(node);
 
 			for(let i = 0; i < children.length; i++) {
-				let myNode = this.copyMatrix(node);
-
 				let x = children[i][0];
 				let y = children[i][1];
 
-				myNode[x][y] = this.computerColor;
-
-				v = Math.max(v, this.minimaxNext(myNode, depth - 1, alpha, beta, false));
+				node[x][y] = this.computerColor;
+				v = Math.max(v, this.minimaxNext(node, depth - 1, alpha, beta, false));
 				alpha = Math.max(alpha, v);
+				node[x][y] = 0;
 
 				if(beta <= alpha) {
 					break;
 				}
 			}
 			return v;
-
 		} else {	// minimizing
 			let v = Number.MAX_SAFE_INTEGER;
 			let children = this.getEmptySlots(node);
 			
 			for(let i = 0; i < children.length; i++) {
-				let myNode = this.copyMatrix(node);
-
 				let x = children[i][0];
 				let y = children[i][1];
 
-				myNode[x][y] = this.playerColor;
-
-				v = Math.min(v, this.minimaxNext(myNode, depth - 1, alpha, beta, true));
+				node[x][y] = this.playerColor;
+				v = Math.min(v, this.minimaxNext(node, depth - 1, alpha, beta, true));
 				beta = Math.min(beta, v);
+				node[x][y] = 0;
 
 				if(beta <= alpha) {
 					break;
